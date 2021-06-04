16f700ff1f794c8b436b079d4797fded12b8f759
Austin Huang <austin.huang@ebaotech.com>
2021-06-04 16:55:54 -0500

    🔨fix upgrade - eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,12 @@ module.exports = {
   parserOptions: {
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
   rules: {},
 };
